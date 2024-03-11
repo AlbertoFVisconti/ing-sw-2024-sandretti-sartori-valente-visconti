6133c7d85308fd025225c12c1fb405175d857cf4
alberto <albertofrancesco.visconti@mail.polimi.it>
2024-03-11 19:12:15 +0100

    added islocked attribute and modified isLocked in GoalDeck.java

--- src/main/java/it/polimi/ingsw/model/goals/GoalDeck.java
+++ src/main/java/it/polimi/ingsw/model/goals/GoalDeck.java
@@ -3,9 +3,12 @@ package it.polimi.ingsw.model.goals;
 import java.util.List;
 
 public class GoalDeck {
+    private boolean isLocked;
     private List<Goal> remaining;
 
-    public void lockDeck() {}
+    public void lockDeck() {
+        isLocked=true;
+    }
 
     public Goal getCard() {
         return null;
