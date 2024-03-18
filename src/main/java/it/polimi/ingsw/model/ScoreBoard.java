4ba845319d0008a519f72eec87299ed8b651655b
massimo sandretti <massimo.sandretti@gmail.com>
2024-03-18 03:17:34 +0100

    closed issue ING-29 docs provided for ScoreBoard class

--- src/main/java/it/polimi/ingsw/model/ScoreBoard.java
+++ src/main/java/it/polimi/ingsw/model/ScoreBoard.java
@@ -4,10 +4,20 @@ import it.polimi.ingsw.model.player.Player;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+/**
+ * The ScoreBoard class represents a scoreboard that tracks players' score throughout the game.
+ */
 public class ScoreBoard {
     private final HashMap<Player, Integer> scores;
 
+    /**
+     * Construct a new ScoreBoard object for the given list of players.
+     * Initial scores are set to 0 for each player.
+     *
+     * @param players A list of Players participating in the game.
+     */
     public ScoreBoard(List<Player> players) {
         scores = new HashMap<>();
 
@@ -16,11 +26,26 @@ public class ScoreBoard {
         }
     }
 
+    /**
+     * Retrieves the score of the specified player from the scoreboard.
+     *
+     * @param player the Player whose score is to be retrieved
+     * @return The score of the specified player.
+     */
     public int getScore(Player player) {
+        if(!scores.containsKey(player)) throw new NoSuchElementException("This player is not in the scoreboard");
         return scores.get(player);
     }
 
+    /**
+     * Sets the score for the specified player in the scoreboard.
+     *
+     * @param player the Player whose score needs to be set.
+     * @param score the new score to set for the player.
+     * @throws NoSuchElementException if the provided player is not in the scoreboard
+     */
     public void setScore(Player player, int score) {
+        if(!scores.containsKey(player)) throw new NoSuchElementException("This player is not in the scoreboard");
         scores.put(player,score);
     }
 }
