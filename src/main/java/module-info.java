2409ea0fe3ce544a75eb98afdefdb977966b2555
MassimoSandre <massimoalbino.sandretti@mail.polimi.it>
2024-05-08 20:31:13 +0200

    added missing exports and opens to make server and clients work again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,11 @@ module it.polimi.ingsw {
     requires org.controlsfx.controls;
     requires java.desktop;
 
+    exports it.polimi.ingsw.network.rmi to java.rmi;
+    exports it.polimi.ingsw.view to java.rmi;
+
     opens it.polimi.ingsw.view.ui to javafx.fxml;
+    opens it.polimi.ingsw.view.FXController to javafx.fxml;
+
     exports it.polimi.ingsw.view.ui;
 }
