4c515be951b5eee2fefcb07059ec47494b2629e8
mavio <mario1.valente@mail.polimi.it>
2024-05-25 21:30:44 +0200

    Now Starting Card are displayed correctly

--- src/main/java/it/polimi/ingsw/view/ui/gui/FXController/StartingCardController.java
+++ src/main/java/it/polimi/ingsw/view/ui/gui/FXController/StartingCardController.java
@@ -2,12 +2,19 @@ package it.polimi.ingsw.view.ui.gui.FXController;
 
 import it.polimi.ingsw.events.messages.client.PlaceStartCardMessage;
 import it.polimi.ingsw.network.Client;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -22,24 +29,42 @@ public class StartingCardController {
         System.out.println(Client.getInstance().getUserInterface().getLocalPlayer().getStartCard().getFrontpath());
         System.out.println(Objects.requireNonNull(getClass().getResource(Client.getInstance().getUserInterface().getLocalPlayer().getStartCard().getFrontpath())).toString());
 
-        startingcardfront= new ImageView(new Image(
+        startingcardfront.setImage(new Image(
                 Objects.requireNonNull(getClass().getResource(Client.getInstance().getUserInterface().getLocalPlayer().getStartCard().getFrontpath())).toString()
         ));
-        startingcardback= new ImageView(new Image(
+        startingcardback.setImage(new Image(
                 Objects.requireNonNull(getClass().getResource(Client.getInstance().getUserInterface().getLocalPlayer().getStartCard().getBackpath())).toString()
         ));
     }
     @FXML
     void SelectFront(MouseEvent event) throws IOException {
-        Client.getInstance().getUserInterface().setSelectedside(0);
-        Client.getInstance().getServerHandler().sendMessage(new PlaceStartCardMessage(false));
-
+        Platform.runLater(() -> {
+            try {
+                Client.getInstance().getUserInterface().setSelectedside(0);
+                Client.getInstance().getServerHandler().sendMessage(new PlaceStartCardMessage(false));
+                Parent nextPageParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/PrivateGoal.fxml")));
+                Stage window = new Stage();
+                window.setScene(new Scene(nextPageParent));
+                window.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
     @FXML
     void SelectBack(MouseEvent event) throws IOException {
-        Client.getInstance().getUserInterface().setSelectedside(1);
-        Client.getInstance().getServerHandler().sendMessage(new PlaceStartCardMessage(true));
-
+        Platform.runLater(() -> {
+            try {
+                Client.getInstance().getUserInterface().setSelectedside(1);
+                Client.getInstance().getServerHandler().sendMessage(new PlaceStartCardMessage(true));
+                Parent nextPageParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/PrivateGoal.fxml")));
+                Stage window = new Stage();
+                window.setScene(new Scene(nextPageParent));
+                window.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
